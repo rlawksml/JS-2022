5864c8ae19d61e6b20edbb1c30c5509094153be8
JINHEE KIM <rlawksml@gmail.com>
2022-01-20 18:56:45 +0900

    step 2  까지 기능구현 완료
    step 2  까지 기능구현 완료

--- index3.js
+++ index3.js
@@ -1,7 +1,23 @@
-(function () {})();
+const $ = (select) => document.querySelector(select);
+
+const menuTemplate = (menuname) => `${menuname}`
+
+function App() {
+
+}
+
+
+let current_menu = 'espresso'
+
+
+window.addEventListener('load', () => {
+  console.log(current_menu)
+  document.getElementById('menu-list').innerHTML = get_localdata(current_menu)
+})
 
 function CountingItems() {
-  const menu_count = document.getElementById('espresso-menu-list').childElementCount;
+  const menu_count = document.getElementById('menu-list').childElementCount;
+  // const menu_count = document.getElementById('menu-list').querySelectorAll('li').length
   document.getElementById('menu-count').innerText = `총 ${menu_count}개`;
 }
 
@@ -9,21 +25,13 @@ function ModifyItem(e) {
   if (confirm('수정하시겠습니까?')) {
     const modified_ItemName = prompt('수정 값을 입력해주세요');
     if (modified_ItemName.length != 0) {
-      e.closest('li').outerHTML = `<li class="menu-list__item">
-      <span class="menu-list__item__name">${modified_ItemName}</span>
-      <button type="button" class="menu-list__item__okBtn" onClick="SoldoutItem(this)">
-      품절
-    </button>
-      <button type="button" class="menu-list__item__okBtn" onClick="ModifyItem(this)">
-        수정
-      </button>
-      <button type="button" class="menu-list__item__cancleBtn" onClick="DeleteItem(this)">
-        삭제
-      </button>
-      </li>`;
+      e.closest('li').outerHTML = MakeMenu_template(modified_ItemName);
     }
   }
   CountingItems();
+
+
+  save_localdata(current_menu, document.getElementById('menu-list').innerHTML)
 }
 
 function DeleteItem(e) {
@@ -31,6 +39,8 @@ function DeleteItem(e) {
     e.closest('li').remove();
   }
   CountingItems();
+
+  save_localdata(current_menu, document.getElementById('menu-list').innerHTML)
 }
 
 function SoldoutItem(e) {
@@ -39,13 +49,12 @@ function SoldoutItem(e) {
   } else {
     e.closest('li').classList.add('sold-out');
   }
+
+  // save_localdata(key,value)
 }
 
-function MakeItem() {
-  const input_value = document.querySelector('#newMenu-input').value;
-  document.querySelector('#newMenu-input').value = '';
-
-  const item = `<li class="menu-list__item">
+function MakeMenu_template(input_value) {
+  const item  = `<li class="menu-list__item">
   <span class="menu-list__item__name">${input_value}</span>
   <button type="button" class="menu-list__item__okBtn" onClick="SoldoutItem(this)">
   품절</button>
@@ -57,7 +66,34 @@ function MakeItem() {
   </button>
   </li>`;
 
-  document.getElementById('espresso-menu-list').insertAdjacentHTML('afterbegin', item);
+  // const item = (input_value)  => `<li class="menu-list__item">
+  // <span class="menu-list__item__name">${input_value}</span>
+  // <button type="button" class="menu-list__item__okBtn" onClick="SoldoutItem(this)">
+  // 품절</button>
+  // <button type="button" class="menu-list__item__okBtn" onClick="ModifyItem(this)">
+  //   수정
+  // </button>
+  // <button type="button" class="menu-list__item__cancleBtn" onClick="DeleteItem(this)">
+  //   삭제
+  // </button>
+  // </li>`;
+
+  return item
+}
+
+function MakeItem() {
+  const input_value = document.querySelector('#newMenu-input').value;
+
+  document.querySelector('#newMenu-input').value = '';
+
+  const item = MakeMenu_template(input_value)
+
+  document.getElementById('menu-list').insertAdjacentHTML('afterbegin', item);
+
+  const value = document.getElementById('menu-list').innerHTML
+
+  // 로컬데이터 저장하기
+  save_localdata(current_menu, value)
 }
 
 document.getElementById('newMenu-input').addEventListener('keyup', (e) => {
@@ -73,3 +109,22 @@ document.querySelector('#input-btn').addEventListener('click', (e) => {
   }
   CountingItems();
 });
+
+
+function select_menu(e) {
+  current_menu = e.dataset.name
+
+  document.getElementById('current_menu_title').innerText = current_menu + " Management"
+
+  document.getElementById('menu-list').innerHTML = get_localdata(current_menu)
+
+  CountingItems();
+}
+
+function save_localdata(key, value) {
+  localStorage.setItem(key, value)
+}
+
+function get_localdata(key) {
+  return localStorage.getItem(key)
+}
